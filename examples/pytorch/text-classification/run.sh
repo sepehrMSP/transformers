7d750ef22db4cd189f5e056200399a6359b5160c
sepehrMSP <sepehrpourghannad@yahoo.com>
2024-03-26 16:32:35 +0100

    Fix task name in run.sh + Bind command to 48 processes

--- examples/pytorch/text-classification/run.sh
+++ examples/pytorch/text-classification/run.sh
@@ -2,6 +2,7 @@
 
 # Create the output folder
 mkdir -p easl_output
+export TASK_NAME=mnli
 
 # Loop through different batch sizes
 for ((i=4; i<=16; i++))
@@ -10,7 +11,17 @@ do
 
     # Run the command with the current batch size
     echo "Running with batch size: $batch_size"
-    python run_glue.py --model_name_or_path google-bert/bert-base-cased --task_name $TASK_NAME --do_train --do_eval --max_seq_length 128 --per_device_train_batch_size $batch_size --learning_rate 2e-5 --num_train_epochs 5 --output_dir out/$TASK_NAME/ | tee easl_output/output_batch_size_${batch_size}.txt
+    taskset -c 0-47 python run_glue.py --model_name_or_path google-bert/bert-base-cased \
+        --task_name $TASK_NAME \
+        --do_train \
+        --do_eval \
+        --max_seq_length 128 \
+        --per_device_train_batch_size $batch_size \
+        --learning_rate 2e-5 \
+        --num_train_epochs 5 \
+        --output_dir out/$TASK_NAME/ \
+        --dataloader_num_workers=48 \
+        | tee easl_output/output_batch_size_${batch_size}.txt
 
     echo "Finished running with batch size: $batch_size"
 done
